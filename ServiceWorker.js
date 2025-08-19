b9108e3be856bd3bce95146b32b41d44c63b9378
Sharanya7705 <110808482+Sharanya7705@users.noreply.github.com>
2025-08-19 19:42:06 +0530

    OGP

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-DefaultGame-1.0";
 const contentToCache = [
-    "Build/Build.loader.js",
-    "Build/Build.framework.js",
-    "Build/Build.data",
-    "Build/Build.wasm",
+    "Build/Gigachad-vs-100-memes.loader.js",
+    "Build/Gigachad-vs-100-memes.framework.js",
+    "Build/Gigachad-vs-100-memes.data",
+    "Build/Gigachad-vs-100-memes.wasm",
     "TemplateData/style.css"
 
 ];
